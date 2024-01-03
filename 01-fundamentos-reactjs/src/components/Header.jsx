974deeffa460c74e28276fa925f909bc78f673fb
Khellermann Machado <khellermann@gmail.com>
2024-01-03 15:15:38 -0300

    Testando repo

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export function Header(){
     return (
         <header className={styles.header}>
-            <h1>Feed Ignite</h1>
+            <h1>Feed Ignite 2024</h1>
         </header>
     )
 }
